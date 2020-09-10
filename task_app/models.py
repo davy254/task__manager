d94b551bea8d84268c2548a34ba4d9a914b56378
David <davidmunyiri2@gmail.com>
2020-09-10 15:21:47 +0300

    Update models.py

--- task_app/models.py
+++ task_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+class Task(models.Model):
+    task_name = models.CharField(max_length=200)
+    date_created = models.DateTimeField(default=timezone.now)
+    date_due = models.DateTimeField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.task_name
+
