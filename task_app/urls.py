d0f464bad81ae2193ba207c3c4d1d96added45b7
David <davidmunyiri2@gmail.com>
2020-09-15 22:37:02 +0300

    views

--- task_app/urls.py
+++ task_app/urls.py
@@ -1,8 +1,21 @@
 from django.urls import path
 from . import views
+from .views import TaskListAPIView, TaskDetailAPIView, GenericAPIView
+
+
 
 urlpatterns = [
+    # to use these paths un comment the function view
     path('', views.home , name='home'),
-    path('task-list', views.task_list),
-    path('detail/<int:pk>/', views.task_detail)
+   # path('task-list', views.TaskAPIView),
+    #path('detail/<int:pk>/', views.TaskAPIView),
+
+    #using clas based views
+    path('tasks-list-api/', TaskListAPIView.as_view() ),
+    path('tasks-detail-api/<int:id>/', TaskDetailAPIView.as_view() ),
+
+    # using generic views
+    path('tasks-generic-api/', GenericAPIView.as_view()),
+    path('tasks-generic-api/<int:pk>/', GenericAPIView.as_view()),
+
 ]
--- task_app/views.py
+++ task_app/views.py
@@ -7,6 +7,16 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
+from rest_framework.views import APIView
+
+from rest_framework.generics import GenericAPIView
+from rest_framework import mixins
+from rest_framework.mixins import (ListModelMixin, CreateModelMixin,
+                                   UpdateModelMixin, RetrieveModelMixin,
+                                   DestroyModelMixin)
+from rest_framework.authentication import  SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+
 
 # Create your views here.
 
@@ -14,7 +24,8 @@ def home(request):
     tasks = Task.objects.all().order_by('date_due')
     return render(request, 'task_app/home.html', {'tasks':tasks})
 
-
+# function Based API views
+"""
 @api_view(['GET', 'POST'])
 def task_list(request):
     if request.method == "GET":
@@ -55,5 +66,74 @@ def task_detail(request, pk):
         task.delete()
         return  Response(status=status.HTTP_204_NO_CONTENT)
 
+"""
+
+# class based API views
+class TaskListAPIView(APIView):
+
+    def get(self, request):
+        tasks = Task.objects.all()
+        serializer = TaskSerializer(tasks, many= True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        data = request.data
+        serializer = TaskSerializer(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+class TaskDetailAPIView(APIView):
+
+    def get_object(self, id):
+        try:
+            return Task.objects.get(id=id)
+        except Task.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def get(self, request, id):
+        task = self.get_object(id)
+        serializer = TaskSerializer(task)
+        return Response(serializer.data)
+
+    def delete(self, request, id):
+        task = self.get_object(id)
+        task.delete()
+        return  Response(status=status.HTTP_204_NOT_FOUND)
+
+    def put(self, request, id):
+        task = self.get_object(id)
+        serializer = TaskSerializer(task, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# using generic views
+class GenericAPIView(GenericAPIView, ListModelMixin,
+                     CreateModelMixin, UpdateModelMixin,
+                     RetrieveModelMixin, DestroyModelMixin):
+
+    serializer_class = TaskSerializer
+    queryset = Task.objects.all()
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, pk=None):
+        if pk:
+            return self.retrieve(request)
+        else:
+            return  self.list(request)
+
+    def post(self, request):
+        return self.create(request)
+
+    def put(self, request, pk=None):
+        return  self.update(request, pk)
+
+    def delete(self, request, pk=None):
+        return self.destroy(request, pk)
